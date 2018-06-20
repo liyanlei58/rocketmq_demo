b898d8c1f9dc699ed7c0372a2bb69fe15186889d
yanleili <2191176072@qq.com>
2018-06-20 11:45:25 +0800

    first commit

---
+++ src/main/java/com/lyl/mq/App.java
@@ -0,0 +1,13 @@
+package com.lyl.mq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/lyl/mq/PushConsumer.java
@@ -0,0 +1,78 @@
+package com.lyl.mq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+
+public class PushConsumer {
+    /**
+     * 当前例子是PushConsumer用法，使用方式给用户感觉是消息从RocketMQ服务器推到了应用客户端。<br>
+     * 但是实际PushConsumer内部是使用长轮询Pull方式从MetaQ服务器拉消息，然后再回调用户Listener方法<br>
+     */
+    public static void main(String[] args) throws InterruptedException,
+            MQClientException {
+        /**
+         * 一个应用创建一个Consumer，由应用来维护此对象，可以设置为全局对象或者单例<br>
+         * 注意：ConsumerGroupName需要由应用来保证唯一
+         */
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(
+                "ConsumerGroupName");
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        consumer.setInstanceName("Consumber");
+
+        /**
+         * 订阅指定topic下tags分别等于TagA或TagC或TagD
+         */
+        consumer.subscribe("TopicTest1", "TagA || TagC || TagD");
+        /**
+         * 订阅指定topic下所有消息<br>
+         * 注意：一个consumer对象可以订阅多个topic
+         */
+        consumer.subscribe("TopicTest2", "*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            public ConsumeConcurrentlyStatus consumeMessage(
+                    List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+
+                System.out.println(Thread.currentThread().getName()
+                        + " Receive New Messages: " + msgs.size());
+
+                MessageExt msg = msgs.get(0);
+                if (msg.getTopic().equals("TopicTest1")) {
+                    //执行TopicTest1的消费逻辑
+                    if (msg.getTags() != null && msg.getTags().equals("TagA")) {
+                        //执行TagA的消费
+                        System.out.println(new String(msg.getBody()));
+                    } else if (msg.getTags() != null
+                            && msg.getTags().equals("TagC")) {
+                        //执行TagC的消费
+                        System.out.println(new String(msg.getBody()));
+                    } else if (msg.getTags() != null
+                            && msg.getTags().equals("TagD")) {
+                        //执行TagD的消费
+                        System.out.println(new String(msg.getBody()));
+                    }
+                } else if (msg.getTopic().equals("TopicTest2")) {
+                    System.out.println(new String(msg.getBody()));
+                }
+
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+
+            }
+        });
+
+        /**
+         * Consumer对象在使用之前必须要调用start初始化，初始化一次即可<br>
+         */
+        consumer.start();
+
+        System.out.println("ConsumerStarted.");
+    }
+}  
